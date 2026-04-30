c1b2b0b7f23fd0499aff0959ae5a87030a848616
Vitor Kaleu <adegawhc@outlook.com>
2026-04-30 12:58:00 -0300

    Primeiro Commit

---
+++ GestaoFinanceira/GestaoFinanceira/Program.cs
@@ -0,0 +1,104 @@
+using GestaoFinanceira.Client.Pages;
+using GestaoFinanceira.Components;
+using GestaoFinanceira.Components.Account;
+using GestaoFinanceira.Data;
+using GestaoFinanceira.libraries.Email;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Mail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration.GetValue<string>("OAuth:Google:ClientId")!;
+        options.ClientSecret = builder.Configuration.GetValue<string>("OAuth:Google:ClientSecret")!;
+    })
+       .AddFacebook(options =>
+       {
+           options.ClientId = builder.Configuration["OAuth:Facebook:ClientId"];
+           options.ClientSecret = builder.Configuration["OAuth:Facebook:ClientSecret"];
+       })
+       .AddMicrosoftAccount(options =>
+       {
+           options.ClientId = builder.Configuration["OAuth:Microsoft:ClientId"];
+           options.ClientSecret = builder.Configuration["OAuth:Microsoft:ClientSecret"];
+       })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<SmtpClient>(options =>
+{
+    var smtp = new SmtpClient();
+    smtp.Host = builder.Configuration.GetValue<string>("EmailSender:Server")!;
+    smtp.Port = builder.Configuration.GetValue<int>("EmailSender:Port");
+    smtp.EnableSsl = builder.Configuration.GetValue<bool>("EmailSender:SSL");
+
+    smtp.Credentials = new NetworkCredential(
+        builder.Configuration.GetValue<string>("EmailSender:User"),
+        builder.Configuration.GetValue<string>("EmailSender:Password")
+        );
+    return smtp;
+}
+    );
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(GestaoFinanceira.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
---
+++ GestaoFinanceira/GestaoFinanceira/libraries/Email/EmailSender.cs
@@ -0,0 +1,50 @@
+﻿using GestaoFinanceira.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Net.Mail;
+
+namespace GestaoFinanceira.libraries.Email
+{
+    public class EmailSender(ILogger<EmailSender> logger, IConfiguration configuration, SmtpClient smtp) : IEmailSender<ApplicationUser>
+    {
+        private readonly ILogger logger = logger;
+        private readonly SmtpClient smtp = smtp;
+
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email,
+            string confirmationLink) => SendEmailAsync(email, "Confirme seu E-mail",
+            "<html lang=\"pt-br\"><head></head><body>Por favor confirme seu cadastro clicando no link " +
+            $"<a href='{confirmationLink}'>Confirme</a>.</body></html>");
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email,
+            string resetLink) => SendEmailAsync(email, "Resetar senha",
+            "<html lang=\"pt-br\"><head></head><body>Clique no link e resete sua senha " +
+            $"<a href='{resetLink}'>Resetar senha</a>.</body></html>");
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email,
+            string resetCode) => SendEmailAsync(email, "Resetar senha",
+            "<html lang=\"pt-br\"><head></head><body>Por favor redefina sua senha " +
+            $"usando o seguinte código:<br>{resetCode}</body></html>");
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            await Execute(subject, message, toEmail);
+        }
+
+        public async Task Execute(string subject, string message, string toEmail)
+        {
+
+            var mailMessage = new MailMessage();
+
+            mailMessage.From = new MailAddress(configuration.GetValue<string>("EmailSender:User")!);
+            mailMessage.Subject = subject;
+            mailMessage.To.Add(new MailAddress(toEmail));
+            mailMessage.Body = message;
+            mailMessage.IsBodyHtml = true;
+
+            await smtp.SendMailAsync(mailMessage);
+
+            logger.LogInformation("E-mail para {EmailAddress} enviado!", toEmail);
+        }
+    }
+
+}

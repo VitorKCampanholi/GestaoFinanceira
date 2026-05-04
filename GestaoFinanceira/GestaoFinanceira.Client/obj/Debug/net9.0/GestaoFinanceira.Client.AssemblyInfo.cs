d86061dfb114bde32d36d4da30fca2e15f3647ba
VitorKCampanholi <adegawhc@outlook.com>
2026-05-04 15:47:32 -0300

    Segundo Commit Layout Lateral e add gitignore

--- GestaoFinanceira/GestaoFinanceira.Client/obj/Debug/net9.0/GestaoFinanceira.Client.AssemblyInfo.cs
+++ GestaoFinanceira/GestaoFinanceira.Client/obj/Debug/net9.0/GestaoFinanceira.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoFinanceira.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b2b0b7f23fd0499aff0959ae5a87030a848616")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoFinanceira.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoFinanceira.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

51ebcd0b125075e536525e64d35e45be291948a0
VitorKCampanholi <adegawhc@outlook.com>
2026-05-29 12:21:00 -0300

    Adicionando mascaras para CNPJ e CEP

---
+++ GestaoFinanceira/GestaoFinanceira/wwwroot/js/app,js.js
@@ -0,0 +1,13 @@
+﻿function CompanyMaskInit() {
+
+
+    var cleaveCNPJ, cleaveCEP;
+
+
+    document.querySelector("#cnpj") && (cleaveDelimiters = new Cleave("#cnpj", { delimiters: [".", ".", "/", "-"], blocks: [2, 3, 3, 4, 2], uppercase: !0 })),
+    document.querySelector("#cep") && (cleaveDelimiters = new Cleave("#cep", { delimiters: [".", "-"], blocks: [2, 3, 3], uppercase: !0 }))
+}
+
+
+
+    

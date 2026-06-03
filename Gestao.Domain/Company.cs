173629558ca9c94c7d1c35bf31ac1cba9e6b58ff
VitorKCampanholi <adegawhc@outlook.com>
2026-06-02 22:33:32 -0300

    Refatorando dados userID

--- Gestao.Domain/Company.cs
+++ Gestao.Domain/Company.cs
@@ -7,27 +7,44 @@ namespace Gestao.Domain
     public class Company
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo 'Razão Social' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O Campo 'Razão Social' deve ter pelo menos {1} caracteres!")]
         public string LegalName { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "O campo 'Nome Fantasia' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O Campo 'Nome Fantasia' deve ter pelo menos {1} caracteres!")]
         public string TradeName { get; set; } = string.Empty;
+
         [CNPJ(ErrorMessage = "O Campo 'CNPJ' é inválido!")]
         [Required(ErrorMessage = "O campo 'CNPJ' é obrigatório!")]
         public string TaxId { get; set; } = string.Empty;
+
+        [MinLength(10, ErrorMessage = "O Campo 'CEP' deve ter {1} caracteres!")]
         [Required(ErrorMessage = "O campo 'CEP' é obrigatório!")]
         public string PostalCode { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "O campo 'Estado' é obrigatório!")]
         public string State { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "O campo 'Cidade' é obrigatório!")]
         public string City { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "O campo 'Bairro' é obrigatório!")]
         public string Neighborhood { get; set; } = string.Empty;
+
+        [MinLength(10, ErrorMessage = "O Campo 'Endereço' deve ter pelo menos {1} caracteres!")]
         [Required(ErrorMessage = "O campo 'Endereço' é obrigatório!")]
         public string Address { get; set; } = string.Empty;
+
+        [MinLength(3, ErrorMessage = "O Campo 'Complemento' deve ter pelo menos {1} caracteres!")]
         [Required(ErrorMessage = "O campo 'Complemento' é obrigatório!")]
         public string Complement { get; set; } = string.Empty;   
+
         public DateTimeOffset CreateDate { get; set; }
+
         public Guid UserId { get; set; }
+
         public ApplicationUser User { get; set; } = null!;
     }
 }
---
+++ GestaoFinanceira/GestaoFinanceira.Client/Helpers/AthenticationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace GestaoFinanceira.Client.Helpers
+{
+    public class AthenticationHelper
+    {
+        public static async Task<Guid?> GetAuthenticationUserIdAsync(AuthenticationStateProvider authenticationStateProvider)
+        {
+
+            var authenticationState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var userId = authenticationState.User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
+
+            if (userId != null && Guid.TryParse(userId!.Value, out var authenticationUserId))
+            {
+                return authenticationUserId;
+            }
+            return null;
+        }
+    }
+}
